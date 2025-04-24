be0ec3b95d7cfcdabae29736409f932cb4ae51b9
Furkan Berkay ÇAKMAK <furkanberkaycakmak@icloud.com>
2025-04-25 02:12:11 +0300

    Analytics integrated on layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
           }}
         />
       </head>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children} <Analytics /> </body>
     </html>
   );
 }
